1f6458f35a2c55c7c8317f12bc40e0a7f191f94b
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-02-23 18:35:22 +0530

    added a text container

---
+++ src/components/TextContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TextContainer = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            {/* Your text containers go here */}
+            <div className="bg-white h-60 w-96 rounded-xl shadow-2xl p-4 m-5">
+                {/* Placeholder text */}
+            </div>
+            <div className="bg-white h-60 w-96 rounded-xl shadow-2xl p-4 m-5">
+                {/* Placeholder text */}
+            </div>
+        </div>
+    );
+};
+
+export default TextContainer;
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,13 @@
 import { AppBar } from "../components/AppBar";
 import { BottomBar } from "../components/BottomBar";
+import TextContainer from "../components/TextContainer";
 
 export const Game = () => {
     return (
         <div className="bg-cover h-screen" style={{ backgroundImage: 'url(images/pexels-fwstudio-172276.jpg)' }}>
             <AppBar />
             <BottomBar />
+            <TextContainer />
         </div>
     );
 }
